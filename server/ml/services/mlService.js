c268506c3aaeeb7c470c0d743973f734456fc36e
isabelpolo26 <isabelhuamanpolo0@gmail.com>
2025-11-12 17:48:47 -0500

    cambios

--- server/ml/services/mlService.js
+++ server/ml/services/mlService.js
@@ -12,7 +12,7 @@ class MLService {
   /**
    * Train sales prediction model
    */
-  async trainSalesModel(salesData, options = {}) {
+  async trainSalesModel(SalesData, options = {}) {
     try {
       // Transform data to required format
       const trainingData = salesData.map((item, index) => ({
